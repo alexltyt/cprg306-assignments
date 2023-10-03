e4e9c4b44ba3aaaedcb3497c3b758f12b082fe2f
alexltyt <alexltytca@gmail.com>
2023-10-03 17:20:31 -0600

    added home link

--- app/week4/page.js
+++ app/week4/page.js
@@ -1,5 +1,6 @@
 "use client";
 import NewItem from "./new-item"
+import Link from 'next/link';
 
 
 export default function Page() {
@@ -8,6 +9,7 @@ export default function Page() {
     return (
         <main>
             <NewItem/>
+            <Link href="/">Home</Link>
         </main>
     )
 

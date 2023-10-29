bdc5ba834aa49fb07108681bbd9cc4b6b55a45f6
alexltyt <alexltytca@gmail.com>
2023-10-29 15:35:00 -0600

    added key value for week 5

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -30,7 +30,7 @@ export default function ItemList() {
             
             {sortedItems.map((item,index,array) => {
                 if(sortType==="name"||sortType==="category"){
-                    return <Item name={item.name} quantity={item.quantity} category={item.category} />
+                    return <Item key={item.name} name={item.name} quantity={item.quantity} category={item.category} />
                 }else if(sortType==="grouped category"){
                     if (index === 0 || item.category !== array[index - 1].category) {
                         const capitalizedCategory = item.category.charAt(0).toUpperCase() + item.category.slice(1);
